92c77d0b3b3d5e816d179861c016545f491d8e52
zzuuckk <102153314+ZzuuCkk@users.noreply.github.com>
2023-04-25 14:43:57 +0400

    done but just one error

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,61 +12,61 @@ setInterval(clock,1000);
 
 // N2
 
-// function mainSlider() {
-//     const slides = document.querySelectorAll(".slide");
-//     const next = document.querySelector(".next");
-//     const prev = document.querySelector(".prev");
-//     const slider = document.querySelector(".slider");
-//     const bullets = document.querySelectorAll(".bullet");
-//     let setIntervalId = null;
+function mainSlider() {
+    const slides = document.querySelectorAll(".slide");
+    const next = document.querySelector(".next");
+    const prev = document.querySelector(".prev");
+    const slider = document.querySelector(".slider");
+    const bullets = document.querySelectorAll(".bullet");
+    let setIntervalId = null;
 
-//     let activeIndex = 0;
+    let activeIndex = 0;
 
-//     function renderSlides() {
-//         slides.forEach((el, i) => {
-//             if (i === activeIndex) {
-//                 el.classList.add("active");
-//             } else {
-//                 el.classList.remove("active");
-//             }
-//         });   
-//     }
+    function renderSlides() {
+        slides.forEach((el, i) => {
+            if (i === activeIndex) {
+                el.classList.add("active");
+            } else {
+                el.classList.remove("active");
+            }
+        });   
+    }
 
-//     function nextFn(direction) {
-//         if(direction === "right"){
-//             if (activeIndex === slides.length - 1) {
-//                 activeIndex = 0;
-//         } else {
-//             activeIndex++;
-//         }
-//     } else if (direction === "left"){
-//         if(activeIndex === 0){
-//             activeIndex = slides.length - 1;
-//         } else {
-//             activeIndex--;
-//         }
-//     }
-//         renderSlides();
-//     }
+    function nextFn(direction) {
+        if(direction === "right"){
+            if (activeIndex === slides.length - 1) {
+                activeIndex = 0;
+        } else {
+            activeIndex++;
+        }
+    } else if (direction === "left"){
+        if(activeIndex === 0){
+            activeIndex = slides.length - 1;
+        } else {
+            activeIndex--;
+        }
+    }
+        renderSlides();
+    }
 
-//     bullets.forEach((el, i) => {
-//         el.addEventListener("click", () => {
-//             activeIndex = i;
-//             renderSlides();
-//             bullets.forEach((el) => {
-//                 el.classList.remove("bullet-active");
-//             });
-//             el.classList.add("bullet-active");
-//         });
-//         if (i === activeIndex) {
-//             el.classList.add("bullet-active");
-//         } else {
-//             el.classList.remove("bullet-active");
-//         }
-//                 if (activeIndex === 0) {
-//             slides[0].classList.add("active");
-//         }  
-//     });
+    bullets.forEach((el, i) => {
+        el.addEventListener("click", () => {
+            activeIndex = i;
+            renderSlides();
+            bullets.forEach((el) => {
+                el.classList.remove("bullet-active");
+            });
+            el.classList.add("bullet-active");
+        });
+        if (i === activeIndex) {
+            el.classList.add("bullet-active");
+        } else {
+            el.classList.remove("bullet-active");
+        }
+                if (activeIndex === 0) {
+            slides[0].classList.add("active");
+        }  
+    });
 
    
 // // ************autoSlider**********************
@@ -94,20 +94,30 @@ setInterval(clock,1000);
 // //     slider.addEventListener("mouseleave", startInterval);
 
 
-//     renderSlides();
-//     bullets();
-// }
+    renderSlides();
+    bullets();
+}
 
-// mainSlider();
+mainSlider();
     
 
 
-
-
-let countDownDate = new Date("Apr 28 , 2023 20:00:00");
-let today = new Date();
+function countDownTime(){
+const myParagraph = document.querySelector(".coundown");
+let countDownDate = new Date("Apr 28 , 2023 20:00:00").getTime();
+let today = new Date().getTime();
 let difference = countDownDate - today;
-let day = countDownDate.getDay() - today.getDay();
+const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
+const targetDay = Math.floor(difference / day);
+const hours = Math.floor((difference % day) / hour);
+const minutes = Math.floor((difference % hour) / minute);
+const seconds = Math.floor((difference % minute) / second);
+myParagraph.innerHTML = `Next Meet Starts In : ${targetDay} days, ${hours} hours,${minutes} minutes, ${seconds} seconds`;
 
-console.log(minute);
+}
+setInterval(countDownTime,1000);
+
